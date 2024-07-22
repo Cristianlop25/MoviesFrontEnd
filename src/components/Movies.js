759953aa80c2aeefc55526f9c1b45d4ecb72bfd7
Cristian Lopez <clopez@knomadmood.com>
2024-07-22 14:27:45 +0200

    Initial commit after lost the original repository
    This repository was lost so i upload all my old code
    This is a front end for a movies app based on an udemy course

---
+++ src/components/Movies.js
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
+
+const Movies = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+
+    const requestOptions = {
+      method: "GET",
+      headers: headers,
+    }
+
+    fetch(`http://localhost:8080/movies`, requestOptions)
+    .then((response)=> response.json())
+    .then((data) => {
+        setMovies(data);
+      })
+    .catch(err => {
+        console.log(err);
+      })
+
+  }, []);
+
+  return (
+    <div>
+      <h2>Movies</h2>
+      <hr />
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>Movie</th>
+            <th>Release Date</th>
+            <th>Rating</th>
+          </tr>
+        </thead>
+        <tbody>
+          {movies.map((movie) => (
+            <tr key={movie.id}>
+              <td>
+                <Link to={`/movies/${movie.id}`}>
+                  {movie.title}
+                </Link>
+              </td>
+              <td>{movie.release_date}</td>
+              <td>{movie.mpaa_rating}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Movies;
