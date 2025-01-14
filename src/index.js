91bab99c4b6c3aefafca80dd5ce251f9fa22ab17
Cristian Lopez <cristiandavid.lopez.ext@circontrol.com>
2025-01-14 03:12:56 +0100

    Fix to update code

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
+import EditMovie from './components/EditMovie';
 import ErrorPage from './components/ErrorPage';
-import Home from './components/Home'
+import Genres from './components/Genres';
+import GraphQL from './components/GraphQL';
+import Home from './components/Home';
+import Login from './components/Login';
+import ManageCatalogue from './components/ManageCatalogue';
 import Movies from './components/Movies';
 import Movie from './components/Movie';
-import Genres from './components/Genres';
-import EditMovie from './components/EditMovie';
-import ManageCatalogue from './components/ManageCatalogue';
-import GraphQL from './components/GrapQL';
-import Login from './components/Login';
 
 const router = createBrowserRouter([
   {
@@ -18,9 +18,7 @@ const router = createBrowserRouter([
     element: <App />,
     errorElement: <ErrorPage />,
     children: [
-      {
-        index: true, element: <Home />
-      },
+      {index: true, element: <Home /> },
       {
         path: "/movies",
         element: <Movies />,
@@ -35,6 +33,10 @@ const router = createBrowserRouter([
       },
       {
         path: "/admin/movie/0",
+        element: <EditMovie />,
+      },
+      {
+        path: "/admin/movie/:id",
         element: <EditMovie />,
       },
       {
@@ -59,4 +61,3 @@ root.render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-
