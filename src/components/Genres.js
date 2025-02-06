9883ab400501e6af6f31a75bb1c33ec2c6491b8d
Cristian Lopez <cristiandavid.lopez.ext@circontrol.com>
2025-02-06 03:33:23 +0100

    One genre component  implemented and show movies right

--- src/components/Genres.js
+++ src/components/Genres.js
@@ -1,10 +1,59 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const Genres = () => {
-  return (
+  const [genres, setGenres] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+
+    const requestOptions = {
+      method: "GET",
+      headers: headers,
+    }
+
+    fetch(`/genres`, requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.error) {
+          setError(data.message);
+        } else {
+          setGenres(data);
+        }
+      })
+      .catch(err => { console.log(err) })
+  })
+
+
+  if (error !== null) {
+    return <div>Error: {error.message}</div>
+  }
+  else {
+
+    return (
       <div>
         <h2>Genres</h2>
-        <hr/>
+        <hr />
+
+        <div className="list-group">
+          {genres.map((genre) => (
+            <Link
+              key={genre.id}
+              className="list-group-item list-group-item-action"
+              to={`/genres/${genre.id}`}
+              state={
+                {
+                  genreName: genre.genre,
+                }
+              }
+            >{genre.genre}</Link>
+          ))}
+        </div>
       </div>
-  )
+    )
+  }
 }
 
 export default Genres;
