2cd85e680269dd5cc3d15d2abd85016744704c09
Cristian Lopez <cristiandavid.lopez.ext@circontrol.com>
2025-02-20 02:35:24 +0100

    Search function implemented

--- src/components/GraphQL.js
+++ src/components/GraphQL.js
@@ -10,10 +10,49 @@ const GraphQL = () => {
 
   // perform a search
   const performSearch = () => {
+    const payload = `
+    {
+      search(titleContains: "${searchTerm}") {
+        id
+        title
+        runtime
+        release_date
+        mpaa_rating
+      }
+    }`;
 
+    const headers = new Headers();
+    headers.append("Content-Type", "application/graphql");
+
+    const requestOptions = {
+      method: 'POST',
+      headers: headers,
+      body: payload,
+    }
+
+    fetch('/graph', requestOptions)
+      .then(response => response.json())
+      .then((response) => {
+        let theList = Object.values(response.data.search);
+        setMovies(theList);
+      })
+      .catch((error) => {
+        console.log(error)
+      })
   }
 
+
   const handleChange = (event) => {
+    event.preventDefault();
+
+    let value = event.target.value;
+    setSearchTerm(value);
+
+    if (value.length > 2) {
+      performSearch();
+    } else {
+      setMovies(fullList);
+    }
 
   }
 
